dedcfd67e0b6e475b3fd96d49d64ee6b273febb6
smoshy15 <axelpradela03@gmail.com>
2025-01-07 14:27:09 -0300

    Agregar archivos de proyecto.

---
+++ Prueba_login/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private Dictionary<string, string> usuarios;
+
+       
+        public Form1(Dictionary<string, string> usuarios)
+        {
+            InitializeComponent();
+            this.usuarios=usuarios;
+        
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Usuario";
+            label2.Text = "Contraseña";
+            button1.Text = "Iniciar";
+
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            string usuario=textBox1.Text;
+            string contraseña=textBox2.Text;
+
+            if (usuarios.ContainsKey(usuario))
+            {
+                if (usuarios[usuario] == contraseña)  
+                {
+                    MessageBox.Show("Inicio exitoso");
+                }
+                else
+                {
+                    MessageBox.Show("contraseña incorrecta");
+                }
+            }
+            else
+            {
+                MessageBox.Show("usuario no encontrado");
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Prueba_login/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_login
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+           
+
+
+            Dictionary<string, string> usuarios = new Dictionary<string, string>();
+
+            usuarios.Add("admin", "1234");
+            usuarios.Add("usuario1", "password1");
+            usuarios.Add("usuario2", "password2");
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(usuarios));
+
+
+        }
+    }
+}
